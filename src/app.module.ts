aa356e80737134fabc8bdd9856dec8821384c62a
Luiz Calazans <luiz.calazans@movile.com>
2022-04-11 21:06:04 -0300

    Add firebase middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
-import { Module } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PreAuthMiddleware } from './auth/preAuthMiddleware';
 
 @Module({
   imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+      consumer.apply(PreAuthMiddleware).forRoutes({
+        path: '*', method: RequestMethod.ALL
+      });
+  }
+}
---
+++ src/auth/preAuthMiddleware.ts
@@ -0,0 +1,61 @@
+/* eslint-disable prettier/prettier */
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response } from 'express';
+import * as firebase from 'firebase-admin'
+import * as serviceAccount from './firebaseAccountService.json'
+
+
+const firebaseConfig = {
+    type: serviceAccount.type,
+    projectId: serviceAccount.project_id,
+    privateKeyId: serviceAccount.private_key_id,
+    privateKey: serviceAccount.private_key,
+    clientEmail: serviceAccount.client_email,
+    clientId: serviceAccount.client_id,
+    authUri: serviceAccount.token_uri,
+    authProviderX509CentUrl: serviceAccount.auth_provider_x509_cert_url,
+    clientC509CertUrl: serviceAccount.client_x509_cert_url
+}
+
+@Injectable()
+export class PreAuthMiddleware implements NestMiddleware {
+
+    private defaultApp: any;
+
+    constructor() {
+        this.defaultApp = firebase.initializeApp({
+            credential: firebase.credential.cert(firebaseConfig),
+
+        });
+    }
+
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    use(req: Request, res: Response, next: Function) {
+        const token = req.headers.authorization;
+        if (token != null && token != '') {
+            this.defaultApp.auth().verifyIdToken(token.replace('Bearer ', ''))
+                .then(async decodedToken => {
+                    const user = {
+                        email: decodedToken.email
+                    }
+                    req['user'] = user;
+                    next();
+                }).catch(error => {
+                    console.error(error);
+                    this.accessDenied(req.url, res);
+                });
+        } else {
+            next();
+        }
+    }
+
+    private accessDenied(url: string, res: Response) {
+        res.status(403).json({
+            statusCode: 403,
+            timestamp: new Date().toISOString(),
+            path: url,
+            message: 'Access Denied'
+        });
+    }
+
+}
